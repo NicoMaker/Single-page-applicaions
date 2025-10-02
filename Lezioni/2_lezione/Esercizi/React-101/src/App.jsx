e6667fcd7493083714f11757fd2003b26c42bf35
NicoMaker <nicola.marano02@gmail.com>
2025-10-02 12:10:26 +0200

    dati

--- Lezioni/2_lezione/Esercizi/React-101/src/App.jsx
+++ Lezioni/2_lezione/Esercizi/React-101/src/App.jsx
@@ -15,6 +15,7 @@ function App() {
     <>
       <button onClick={clickHandler}>Click me</button>
       <h1 className= {count % 2 === 0 ? "pippo" : "pluto"}>{count}</h1>
+      <h2 className="ciccio"> Ciao</h2>
     </>
   );
 }
