8a2560e7e680f8c037ed672dce5ea6bc4362341f
NicoMaker <nicola.marano02@gmail.com>
2025-11-03 10:40:13 +0100

    ff

--- Lezioni/6_lezione/Esercizi/Form/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import RegistrationForm from "@/components/RegistrationForm"
-
-export default function Page() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-background via-background to-accent/5">
-      <RegistrationForm />
-    </div>
-  )
-}
